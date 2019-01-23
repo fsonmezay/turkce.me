91e52f3baf8b6227c0874ac0647b9b4bb598419d
Ferdi Sönmezay <fsonmezay@gmail.com>
2019-01-23 23:34:58 +0300

    fixed twitter share button

--- js/turkceme.js
+++ js/turkceme.js
@@ -36,6 +36,48 @@
 	  };
 	});
 
+	//http://jasonwatmore.com/post/2014/08/01/angularjs-directives-for-social-sharing-buttons-facebook-like-google-plus-twitter-and-pinterest
+	app.directive('tweet', ['$window', '$location', function ($window, $location) {
+				return {
+						restrict: 'A',
+						scope: {
+								tweet: '=',
+								tweetUrl: '='
+						},
+						link: function (scope, element, attrs) {
+								if (!$window.twttr) {
+										// Load Twitter SDK if not already loaded
+										$.getScript('//platform.twitter.com/widgets.js', function () {
+												renderTweetButton();
+										});
+								} else {
+										renderTweetButton();
+								}
+
+								var watchAdded = false;
+								function renderTweetButton() {
+										if (!scope.tweet && !watchAdded) {
+												// wait for data if it hasn't loaded yet
+												watchAdded = true;
+												var unbindWatch = scope.$watch('tweet', function (newValue, oldValue) {
+														if (newValue) {
+																renderTweetButton();
+															 
+																// only need to run once
+																unbindWatch();
+														}
+												});
+												return;
+										} else {
+												element.html('<a href="https://twitter.com/share" class="twitter-share-button" data-text="' + scope.tweet + '" data-url="' + $location.absUrl() + '" data-hashtags="plazaTurkcesi,turkce">Tweet</a>');
+												$window.twttr.widgets.load(element.parent()[0]);
+										}
+								}
+						}
+				};
+		}
+]);
+
 	app.controller('FormController', ['$rootScope', '$scope', '$http', '$location', function ($rootScope, $scope, $http, $location) {
 		$scope.locutions = [""];
 		$scope.secure = "";
@@ -87,6 +129,7 @@
 //		$routeParams.item
 		$http.get('get?key='+$routeParams.item).then(function(response) {
 			$scope.selectedItem = response.data;
+			$scope.twitterText = 'Plaza Türkçesi\'ni bırakıyoruz! `' + $scope.selectedItem.locution.name + '` demiyoruz.';
 			if($scope.selectedItem.locution.name == null) {
 				$scope.close();
 			}
